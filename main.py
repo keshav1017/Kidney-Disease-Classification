6608d936761ddc8a48b6fe5dea8ee2fee547389e
keshav1017 <prasadkeshav003@gmail.com>
2025-06-22 18:00:19 +0530

    Prepare Base model added

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from cnnClassifier import logging, MyException
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from cnnClassifier.pipeline.stage_02_prepare_base_mode import PrepareBaseModelTrainingPipeline
 import sys
 
 STAGE_NAME = "Data Ingestion"
-
 try:
     logging.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<")
     obj = DataIngestionTrainingPipeline()
     obj.main()
-    logging.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<\n\nx===================x")
+    logging.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<\n\n==========================================================================================================================================\n")
 except Exception as e:
     raise MyException(e, sys) from e 
+
+
+STAGE_NAME = "Prepare base model"
+try:
+    logging.info(f">>>>>> Stage: {STAGE_NAME} started <<<<<<")
+    obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logging.info(f">>>>>> Stage: {STAGE_NAME} completed <<<<<<\n\n==========================================================================================================================================\n")
+except Exception as e:
+    raise MyException(e, sys) from e
